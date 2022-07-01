1d25ffcfcebb199059198963248761b993dd7935
Vladimir Savinov <noonv13@gmail.com>
2022-07-01 16:43:53 +0200

    add files

---
+++ convert_labelme2yolo.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+convert LabelMe JSON to YOLO txt
+
+1. create dirs output, output/images and output/labels,
+2. copy images with json file to output/images directory,
+3. create file_name.txt in output/labels directory with YOLO format.
+
+tested for:
+labelme 3.16.7, 4.6.0, 5.0.1
+
+example:
+./convert_labelme2yolo.py --input=./t8 --output=./res --classes=./class_names.txt
+
+'''
+
+__author__ = 'Vladimir'
+
+import os
+import json
+import cv2
+import glob
+import argparse
+import shutil
+import base64
+import numpy as np
+
+# box form[x,y,w,h]
+def convert(size, box):
+    # x_center y_center width height
+    dw = 1. / size[0]
+    dh = 1. / size[1]
+    x = (box[0] + box[2]) * dw / 2
+    y = (box[1] + box[3]) * dh / 2
+    w = (box[2] - box[0]) * dw
+    h = (box[3] - box[1]) * dh
+    return (x, y, w, h)
+
+def main():
+
+    # parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", required=True, dest="input", default="./4/", type=str, 
+        help="path to directory with images and LabelMe JSON files")
+    ap.add_argument("-o", "--output", required=True, dest="output", default="./res/", type=str, 
+        help="path to directory for store results")
+    ap.add_argument("-c", "--classes", dest="classes", default="./class_names.txt", type=str, 
+        help="file with class labels")
+    args = vars(ap.parse_args())
+    #print(args)
+
+    print("Start...")
+
+    # get JSON files
+    files = sorted( glob.glob( os.path.join(args['input'], '*.json') ) )
+    print("LabelMe files:", files)
+
+    # read classes from file
+    with open(args['classes']) as fs:
+        classes = fs.read().splitlines()
+    print("class_names:",classes)
+
+    # create dirs
+    dst_images_dir = os.path.join(args['output'], "images")
+    dst_labels_dir = os.path.join(args['output'], "labels")
+    if not os.path.exists(args['output']):
+        os.makedirs(args['output'])
+    if not os.path.exists(dst_images_dir):
+        os.makedirs(dst_images_dir)
+    if not os.path.exists(dst_labels_dir):
+        os.makedirs(dst_labels_dir)
+
+    for filename in files:
+        print(filename)
+        with open(filename, 'r') as fs:
+            dataj = json.load(fs)
+
+            image_name = dataj["imagePath"]
+            print("Read", image_name)
+            image_path = os.path.join(args['input'], image_name)
+            image = cv2.imread( image_path )
+            if image is None:
+                print("Error read image by imagePath:", image_path)
+                # try by name
+                image_name = os.path.splitext( os.path.basename(filename) )[0] + ".jpg"
+                image_path = os.path.join(args['input'], image_name)
+                print("Try read image by name:", image_path)
+                image = cv2.imread( image_path )
+                if image is None:
+                    print("Error read image by name:", image_path)
+                    if dataj["imageData"]:
+                        print("Get image data from imageData")
+                        image_b64_data = dataj["imageData"]
+                        image_bin_data = base64.b64decode(image_b64_data)
+                        image = cv2.imdecode( np.asarray(bytearray(image_bin_data), dtype=np.uint8), flags=cv2.IMREAD_COLOR)
+                        print("Decoded image:", image.shape)
+                        print("Save image", image_path)
+                        cv2.imwrite(image_path, image)
+
+            #print(image.shape)
+            height, width = image.shape[:2]
+            print("Image size:", width, height)
+
+            # copy image
+            dst_image_path = os.path.join(dst_images_dir, image_name)
+            print("Copy image to:", dst_image_path)
+            shutil.copyfile(image_path, dst_image_path)
+
+            shapes = dataj["shapes"]
+
+            res_file_name = os.path.splitext( os.path.basename(filename) )[0] + ".txt"
+            print(res_file_name)
+            
+            dst_label_path = os.path.join(dst_labels_dir, res_file_name)
+            try:
+                fw = open(dst_label_path, "a+")
+            except:
+                print("Error open file!")
+
+            for item in shapes:
+                item_class = item["label"]
+                class_id = classes.index(item_class)
+                point1, point2 = item["points"][0], item["points"][1]
+                print(point1, point2)
+
+                x1, y1 = point1
+                x2, y2 = point2
+                xmin = min(x1,x2)
+                xmax = max(x1,x2)
+                ymin = min(y1,y2)
+                ymax = max(y1,y2)
+
+                print("MM:", xmin, xmax, ymin, ymax)
+                box = (xmin, ymin, xmax, ymax)
+
+                #box = point1[0], point1[1], point2[0], point2[1]
+                print(item_class, class_id, box)
+                bb = convert((width, height), box)
+                # class_idx x_center y_center width height
+                out_str = F"{class_id} {bb[0]} {bb[1]} {bb[2]} {bb[3]}\n"
+                print(out_str)
+                fw.write(out_str)
+            fw.close()
+    print("Done.")
+
+if __name__ == '__main__':
+    main()
---
+++ predetect_yolo_to_labelme.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+make detection on image and store results into LabelMe JSON format 
+for next manual labeling
+
+example:
+./predetect_yolo_to_labelme.py --input=./photos/ --model=./yolov5s.pt --classes=./coco_class_names.txt --threshold=0.3
+./predetect_yolo_to_labelme.py --input=./photos --model=../yolo_test/runs/train/exp27/weights/best.pt --classes=./class_names.txt --threshold=0.3
+
+'''
+
+__author__ = 'Vladimir'
+
+import os
+import json
+import cv2
+import glob
+import argparse
+import shutil
+import base64
+import numpy as np
+import importlib
+
+#import labelme
+#import yolov5
+
+try:
+    import labelme
+except ModuleNotFoundError:
+    is_labelme_available = False
+
+try:
+    import yolov5
+    import torch
+except ModuleNotFoundError:
+    is_yolov5_available = False
+
+device = "cuda:0"  # or "cpu"
+
+
+def check_module_available(module_name):
+    module_spec = importlib.util.find_spec(module_name)
+    found = module_spec is not None
+    return found
+
+
+def load_model(model_path):
+    # init yolov5 model
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print('device:', device)
+
+    if torch.cuda.is_available():
+        print('CUDA device name:', torch.cuda.get_device_name())
+
+    yolo = yolov5.YOLOv5(model_path, device)
+    return yolo
+
+
+def model_predict(model, filename, size):
+    # predict
+    results = model.predict(filename, size=size)
+    # print(results)
+    # print(results.pandas().xyxy[0])
+
+    # parse results
+    predictions = results.pred[0]
+
+    #boxes = predictions[:, :4]
+    #scores = predictions[:, 4]
+    #categories = predictions[:, 5]
+    #print(boxes, scores, categories)
+
+    return predictions
+
+
+def convert_predictions_to_shapes(predictions, class_names, threshold):
+    # convert yolo predictions to labelme shapes
+    shapes = []
+
+    pr = predictions.cpu().numpy()
+    # n,6 -> x1, y1, x2, y2, score, category
+    #print(type(pr), pr.shape, pr)
+
+    if pr.size == 0:
+        print("No any predictions!")
+        return []
+
+    for row in pr:
+        print("prediction:", row)
+        x1, y1 = float(row[0]), float(row[1])
+        x2, y2 = float(row[2]), float(row[3])
+        points = [(x1, y1), (x2, y2)]
+        score = row[4]
+        label = class_names[int(row[5])]
+        shape = dict(
+            label=label,
+            points=points,  # [(p[0], p[1]) for p in points],
+            group_id=None,
+            shape_type="rectangle",
+            flags={},
+        )
+        if score >= threshold:
+            print(shape)
+            shapes.append(shape)
+        else:
+            print("Low score for shape:", score)
+
+    return shapes
+
+
+def save_data_to_json(
+        is_labelme_available,
+        image_filename,
+        filename,
+        shapes,
+        imagePath,
+        imageHeight,
+        imageWidth):
+    # save data to JSON via labelme functions or by hands
+    if is_labelme_available:
+        # save data via labelme
+        imageData = labelme.LabelFile.load_image_file(image_filename)
+        labelFile = labelme.LabelFile()
+
+        try:
+            labelFile.save(
+                filename=filename,
+                shapes=shapes,
+                imagePath=imagePath,
+                imageData=imageData,
+                imageHeight=imageHeight,
+                imageWidth=imageWidth)
+        except Exception as e:
+            print("Error write json-file:", filename)
+            raise e
+    else:
+        # save data manually
+
+        # set fields
+        dataj = {}
+        dataj["version"] = None
+        dataj["flags"] = {}
+        dataj["shapes"] = shapes
+        dataj["imagePath"] = imagePath
+        # base64.b64encode(cv2.imread(image_filename)).decode("utf-8")  # base64.b64encode(image).decode("utf-8")
+        dataj["imageData"] = None
+        dataj["imageHeight"] = imageHeight
+        dataj["imageWidth"] = imageWidth
+
+        # print(dataj)
+
+        try:
+            # save JSON data into file
+            with open(filename, 'w') as fs:
+                json.dump(dataj, fs, ensure_ascii=False, indent=2)
+        except Exception as e:
+            print("Error write file:", filename)
+            raise e
+
+    return 0
+
+
+def main():
+    # parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", required=True, dest="input", default="./photos", type=str,
+                    help="path to directory with images")
+    ap.add_argument("-c", "--classes", dest="classes", default="./class_names.txt", type=str,
+                    help="file with class labels")
+    ap.add_argument("-m", "--model", dest="model", default="./yolov5s.pt", type=str,
+                    help="model fot detection")
+    ap.add_argument("-t", "--threshold", dest="threshold", default=0.3, type=float,
+                    help="threshold for model detection")
+    args = vars(ap.parse_args())
+    print(args)
+
+    print("Start...")
+
+    # check - is labelme available?
+    is_labelme_available = False
+    if check_module_available("labelme"):
+        #import labelme
+        is_labelme_available = True
+        print("labelme found!")
+    else:
+        print("labelme not found!")
+
+    # check - is yolov5 available?
+    is_yolov5_available = False
+    if check_module_available("yolov5"):
+        #import yolov5
+        is_yolov5_available = True
+        print("yolov5 found!")
+    else:
+        print("yolov5 not found!")
+
+    # load yolo model
+    if is_yolov5_available:
+        model_path = args['model']
+        model = load_model(model_path)
+
+    # get image files
+    files = sorted(glob.glob(os.path.join(args['input'], '*.jpg')))
+    print("Images:", files)
+
+    # read classes from file
+    with open(args['classes']) as fs:
+        classes = fs.read().splitlines()
+    print("class_names:", classes)
+
+    for filename in files:
+        print("Read", filename)
+
+        res_file_name = os.path.splitext(
+            os.path.basename(filename))[0] + ".json"
+        dst_file_path = os.path.join(args['input'], res_file_name)
+
+        # read image and get it size
+        image = cv2.imread(filename)
+        if image is None:
+            print("Error read image:", filename)
+
+        height, width = image.shape[:2]
+        print("Image size:", width, height)
+
+        shapes = []
+
+        # detect objects here
+        predictions = None
+        if is_yolov5_available:
+            predictions = model_predict(
+                model, filename, size=max(width, height))
+
+            shapes = convert_predictions_to_shapes(
+                predictions, class_names=classes, threshold=args['threshold'])
+
+        # print(shapes)
+
+        if len(shapes) == 0:
+            print("No shapes for save!")
+            continue
+
+        # save to JSON-file
+        imagePath = os.path.basename(filename)
+
+        save_data_to_json(is_labelme_available=is_labelme_available,
+                          image_filename=filename,
+                          filename=dst_file_path,
+                          shapes=shapes,
+                          imagePath=imagePath,
+                          imageHeight=height,
+                          imageWidth=width)
+
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
