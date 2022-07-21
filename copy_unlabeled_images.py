bf9ac6112711d13b991ffef4101a41ee44fe5861
Vladimir Savinov <noonv13@gmail.com>
2022-07-21 15:03:39 +0200

    add script for copy unlabeled images

---
+++ copy_unlabeled_images.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+copy images without LabelMe JSON files to output directory
+
+1. create output directory
+2. copy images without json file to output directory
+
+example:
+copy_unlabeled_images.py --input=./images --output=./unlabeled --extention="jpg"
+
+'''
+
+__author__ = 'Vladimir'
+
+import os
+import glob
+import argparse
+import shutil
+
+
+def main():
+
+    # parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", required=True, dest="input", default="images", type=str,
+                    help="path to directory with images and LabelMe JSON files")
+    ap.add_argument("-o", "--output", dest="output", default="unlabeled", type=str,
+                    help="path to directory for store images without labels")
+    ap.add_argument("-e", "--extention", dest="extention", default="jpg", type=str,
+                    help="extention of image files")
+    args = vars(ap.parse_args())
+    # print(args)
+
+    print("Start...")
+
+    # get JSON files
+    json_files = sorted(glob.glob(os.path.join(args['input'], '*.json')))
+    print("LabelMe files:", json_files)
+
+    # check extention
+    ext = args['extention']
+    if ext.startswith('*'):
+        ext = ext[1:]
+    if ext.startswith('.'):
+        ext = ext[1:]
+    print("Image extention:", ext)
+
+    # get image files
+    image_files = sorted(glob.glob(os.path.join(
+        args['input'], "*." + ext)))
+    print("Image files:", image_files)
+
+    # create list of image filenames with LabelMe JSON files
+    images_with_labels = []
+    for json_filename in json_files:
+        base_name = os.path.splitext(os.path.basename(json_filename))[0]
+        image_filename_with_json = base_name + "." + ext
+        image_path = os.path.join(args['input'], image_filename_with_json)
+        images_with_labels.append(image_path)
+    print("Images with labels:", images_with_labels)
+
+    # create dirs
+    dst_images_dir = args['output']
+    os.makedirs(dst_images_dir, exist_ok=True)
+
+    print("Process images...")
+    for image_filename in image_files:
+        print(image_filename)
+        if image_filename in images_with_labels:
+            print("Has label.")
+        else:
+            print("No label.")
+
+            # copy image
+            dst_image_path = os.path.join(
+                dst_images_dir, os.path.basename(image_filename))
+            print("Copy image to:", dst_image_path)
+            shutil.copyfile(image_filename, dst_image_path)
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
